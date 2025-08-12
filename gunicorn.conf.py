f9ce4a41585ef785edd18047d95fdff096998520
sangho-coder <sanghoroh@binarity.co.kr>
2025-08-12 10:52:03 +0900

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,6 @@
-# gunicorn.conf.py
-import os  # 반드시 추가해야 합니다!
+import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', '8080')}"
-workers = 1
-timeout = 120
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"
+workers = 1  # 무료 티어에서는 1로 고정
+timeout = 30
+preload_app = True  # 빠른 시작
