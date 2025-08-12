fe542e011c7993e6d5a5bdc1be077fdbf9a4861c
sangho-coder <sanghoroh@binarity.co.kr>
2025-08-12 10:02:42 +0900

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,6 @@
-# gunicorn.conf.py
 import os
+
 bind = f"0.0.0.0:{os.getenv('PORT', '8080')}"
 workers = 1
-timeout = 120
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"
+threads = 1
+preload_app = False
